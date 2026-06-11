138b5581fe6d97cd7f39b381836d85dc96ddbdf5
Victor <vherrb97@gmail.com>
2026-06-11 10:08:41 +0200

    Settings basicos

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import inicio
+
+urlpatterns = [
+
+    path(
+        '',
+        inicio,
+        name='inicio'
+    ),
+
+]
---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def inicio(request):
+
+    return render(
+        request,
+        'core/inicio.html'
+    )
