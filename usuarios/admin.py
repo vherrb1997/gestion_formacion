09ef0b546c8869db3f6559f84b8cd4f28c92529a
Victor <vherrb97@gmail.com>
2026-06-12 10:57:03 +0200

    Normalizando codigo

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin # 1. Importar UserAdmin
 from .models import Usuario
 
-admin.site.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (('Información Adicional', {'fields': ('tipo',)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+    ('Información Adicional', {'fields': ('tipo',)}),)
+    list_display = ('username', 'email', 'tipo', 'is_staff', 'is_active')
+
+admin.site.register(Usuario, UsuarioAdmin)
